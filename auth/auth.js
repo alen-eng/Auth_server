d7d2ee94969744b9acb58e2fb7bdd50a12e3bc3a
alen-eng <alenvarghese201@gmail.com>
2022-09-21 13:50:44 +0530

    make it better

--- auth/auth.js
+++ auth/auth.js
@@ -120,7 +120,7 @@ authRouter.post("/parentcheck", async(req,res)=>{
         let batt = parent.battery.toString();
      return res.status(200).json({msg:"PARENT",child:parent.child,location:parent.location['locality'],state:parent.location['administrativeArea'],battery:batt,operator:parent.operator})}
      else
-      return res.status(200).json({msg:"PARENT",child:parent.child,location:"Nil",battery:"Nil",operator:"Nil"})
+      return res.status(200).json({msg:"PARENT",child:parent.child,location:"Nil",state:"Nil",battery:"Nil",operator:"Nil"})
  }
  else if(child){
      return res.status(200).json({msg:"CHILD",parent:child.parent})
