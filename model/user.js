5486af1f790a4c968d681633e6c64c7326808ee1
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-08-24 23:52:27 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,56 +1,5 @@
-// const mongoose=require("mongoose")
-
-// const userSchema=mongoose.Schema({
-
-//     phone:{
-//         required:true,
-//         type:Number,
-//         trim:true,
-//         validate:{
-//             validator:(val)=>{
-//                 const re = /^\d{10}$/
-                
-//                 return val.match(re)
-//             },
-//             message:'incorrect number format'
-
-//         }
-//     },
-//     password:{
-//         required:true,
-//         type:String,
-        
-//     }
-
-// })
-// const User=mongoose.model('user',userSchema)
-// module.exports=User
-
-
 module.exports={
-    USER_COLLECTION:'users',
-    CHILD_COLLECTION:'child'
+    USER_COLLECTION:'credentials',
+    GOOGLE_COLLECTION:'google',
+    GITHUB_COLLECTION:'github',
 }
-
-
-// const mongoClient= require('mongodb').MongoClient
-// const state ={
-//     db:null
-// }
-// module.exports.connect=function(done){
-//     //const Password=process.env.tvZwiltv8ylWVSkA
-//    const url= 'mongodb+srv://Admin-007:tvZwiItv8yIWVSkA@cluster-007.kxziv.mongodb.net/Cluster-007';
-//     //const url='mongodb://localhost:27017'
-//     const dbname='trackapp'
-
-//     mongoClient.connect(url,(err,data)=>{
-//         if(err) return done(err)
-//         state.db=data.db(dbname)
-//         done()
-//     })
-
-// }
-
-// module.exports.get=function(){
-//     return state.db
-// }
