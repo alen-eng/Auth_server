46423cda5eacf39eb8c972b3fdfbdf5c5e6f3697
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-08-24 23:54:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,97 +1,32 @@
 const authRouter=require("./auth/auth.js")
 require('dotenv').config()
 const express=require("express")
-const mongoose=require("mongoose")
 const http=require('http')
-const User = require("./model/user.js")
-const { USER_COLLECTION } = require("./model/user.js")
 
+const cors = require('cors')
 const app =express()
 const server=http.createServer(app)
 const io=require('socket.io')(server)
-
-const mongoClient= require('mongodb').MongoClient
-
-const PORT=process.env.PORT || 3000
-
 const db=require("./connect")
+const PORT=process.env.PORT || 3200
+
 
 db.connect((err)=>{
     if(err)
       console.log('Database not connected!!'+err);
     else
     console.log('Database connected successfully!!');
-  })
+  });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
-// const DB=process.env.DB_PASS
 
-// //middleware
  app.use(express.json())
- //app.use(io)
+ app.use(cors());
  app.set("io", io)
  app.use(authRouter)
-// //
 
 server.listen(PORT,()=>{
     console.log(`connected to PORT : ${PORT}`)
 })
-//(DB).then(()=>{
-//     console.log("connection created")
-// }).catch((e)=>{
-//         console.log(e)
-// })
-
-
-// const state ={
-//     db:null
-// }
-// module.exports.connect=function(done){
-//     //const Password=process.env.tvZwiltv8ylWVSkA
-//    const url= 'mongodb+srv://rangeappuser:2orL6IKcLoKpTkfD@cluster-007.kxziv.mongodb.net/Cluster-007';
-//     //const url='mongodb://localhost:27017'
-//     const dbname='trackapp'
-//     // mongoose.connect(url,(err,data)=>{
-//     //     if(err) return done(err)
-//     //     else console.log("connected");
-//     //     state.db=data.db(dbname)
-//     //     done()
-//     // })
-//     mongoClient.connect(url,(err,data)=>{
-//         if(err) return done(err)
-//         else console.log('connected')
-//         state.db=data.db(dbname)
-//         done()
-//     })
-// }
-
-
-
-// module.exports.get=function(){
-//     return state.db
-// }
-
-
-
-
-
-
-
-// io.on("connection",(socket)=>{
-//     console.log("connected to socket")
-
-//     socket.on('/listenDB',async (msg)=>{
-//         console.log(msg)
-       
-//             socket.emit('/message',"hi")
-//         })
-// })
-
-//   USER_COLLECTION.watch().on('change',async(data)=>{
-    
-//       var users=await User.find()
-//       io.emit("/datachange",users)
-
-//   })
