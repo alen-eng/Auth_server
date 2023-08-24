751800d606ac543248a50e5e71b7826802657f5a
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-08-24 23:53:04 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,26 +1,14 @@
-// const db=require("./index")
-
-// db.connect((err)=>{
-//     if(err)
-//       console.log('Database not connected!!'+err);
-//     else
-//     console.log('Database connected successfully!!');
-//   })
+const redis = require('redis')
+const Redis = require("ioredis");
 const mongoClient= require('mongodb').MongoClient
 const state ={
   db:null
 }
 module.exports.connect=function(done){
-  //const Password=process.env.tvZwiltv8ylWVSkA
- const url= 'mongodb+srv://rangeappuser:2orL6IKcLoKpTkfD@cluster-007.kxziv.mongodb.net/Cluster-007';
-  //const url='mongodb://localhost:27017'
-  const dbname='trackapp'
-  // mongoose.connect(url,(err,data)=>{
-  //     if(err) return done(err)
-  //     else console.log("connected");
-  //     state.db=data.db(dbname)
-  //     done()
-  // })
+
+ const url=process.env.MONGODB_URL
+  const dbname='Auth'
+ 
   mongoClient.connect(url,(err,data)=>{
       if(err) return done(err)
       else console.log('connected')
@@ -28,8 +16,6 @@ module.exports.connect=function(done){
       done()
   })
 }
-
-
 
 module.exports.get=function(){
   return state.db
