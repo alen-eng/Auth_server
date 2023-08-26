a3055f299f97fecbb2226698d626ba1df3bef994
ALEN THOMAS VARUGHESE <85294601+alen-eng@users.noreply.github.com>
2023-08-26 16:06:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ app.get('/', (req, res) => {
  app.use(cors({
   origin: '*'
 }));
+
+app.options('*', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+});
+app.use((req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+});
+
  app.set("io", io)
  app.use(authRouter)
 
